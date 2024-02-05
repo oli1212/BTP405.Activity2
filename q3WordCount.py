c42492a891c393401b1f41008f2b9b76a2c6f610
Oscar Li <oli12myseneca.ca>
2024-02-04 23:08:46 -0500

    finished wordCount()

--- q3WordCount.py
+++ q3WordCount.py
@@ -1,5 +1,18 @@
 def wordCount(t):
-    return
+    dictionary = {}
+    file = open(t, "r")
+    
+    for lineNum, line in enumerate(file, start=1):
+        words = line.replace(',','').replace('.','').strip().split()
+        # set will get unique words in the text
+        for word in set(words):
+            if word not in dictionary:
+                dictionary[word] = [lineNum]
+            elif lineNum not in dictionary[word]:
+                dictionary[word].append(lineNum)
 
+    return dictionary
 
-wordCount("./q3Text.txt")
+# Print the result
+for word, lineNum in wordCount("./q3Text.txt").items():
+    print(f"{word}: {lineNum}")
